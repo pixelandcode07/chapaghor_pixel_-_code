c36bc22c45405fa87c992464be08c0caa7a2b6f1
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2026-08-12 17:34:11 +0600

    categoryy butn add

--- src/components/home/(Grafich)/Grafich.tsx
+++
@@ -1,9 +0,0 @@
-import TestDesign from './Components/test';
-
-export default function GrafichTest() {
-  return (
-    <div>
-      <TestDesign />
-    </div>
-  );
-}
--- src/components/home/(GraphicResources)/GraphicWrap.tsx
+++ src/components/home/(GraphicResources)/GraphicWrap.tsx
@@ -1,13 +1,17 @@
+import BrowseBtn from './components/BrowseBtn';
 import GraphicCard from './components/GraphicCard';
+import GraphicCategory from './components/GraphicCategory';
 import GraphicHead from './components/GraphicHead';
 
 export default function GraphicResources() {
   return (
-    // <div className="container-custom bg-[linear-gradient(180deg,rgba(229,238,249,0.6)_0%,rgba(255,255,255,0)_100%)]">
-    <div className="container-custom  ">
-      <GraphicHead />
-
-      <GraphicCard />
+    <div className=" mt-8 bg-[linear-gradient(180deg,rgba(229,238,249,0.6)_0%,rgba(255,255,255,0)_100%)]">
+      <div className="container-custom  ">
+        <GraphicHead />
+        <GraphicCategory />
+        <GraphicCard />
+        <BrowseBtn />
+      </div>
     </div>
   );
 }
