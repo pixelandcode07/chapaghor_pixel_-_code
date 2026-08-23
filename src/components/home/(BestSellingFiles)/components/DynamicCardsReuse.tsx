21c88bcc4766f5ddbab4e0cb87da0cc51e061188
Moinuddin-dotcom <moinchy7@gmail.com>
2026-08-23 17:37:08 +0600

    text size updated

--- src/components/home/(BestSellingFiles)/components/DynamicCardsReuse.tsx
+++ src/components/home/(BestSellingFiles)/components/DynamicCardsReuse.tsx
@@ -106,7 +106,7 @@ export default function DynamicCardsReuse({
               className={`bg-[#F4F4F5] text-[#012C60] font-normal rounded-[8px] xl:rounded-[10px] group-hover:bg-[#E5E7EB] transition-colors text-center truncate block ${
                 isHorizontal
                   ? "w-full text-xs p-1 lg:p-1.5"
-                  : "w-full text-[13px] md:text-[10px] xl:text-[12px] 3xl:text-[18px] p-1 lg:py-1 tracking-[0.5px]"
+                  : "w-full text-[13px] md:text-[10px] xl:text-[12px] 3xl:text-[24px] p-1 lg:py-1 tracking-[0.5px]"
               }`}
             >
               {product.name}
