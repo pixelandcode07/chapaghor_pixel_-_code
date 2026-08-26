0b982b47c7294f7caaa672d6db2c913849932dfe
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-08-26 18:12:25 +0600

    Update: src/components/home/(ChooseUs)/components/choose-order-inquery.tsx

--- src/components/home/(ChooseUs)/components/choose-order-inquery.tsx
+++ src/components/home/(ChooseUs)/components/choose-order-inquery.tsx
@@ -8,7 +8,7 @@ export default function CustomOrderInquiry() {
   return (
     <section className="w-full">
       {/* ========================= Mobile / Tablet / LG Full Width ========================= */}
-      <div className="flex w-full items-center justify-between gap-2 rounded-[8px] bg-[linear-gradient(to_right,#FEF1E8_0%,#FFFFFF_50%,#FEF1E8_100%)] px-2 py-3 shadow-[0_4px_12px_rgba(0,0,0,0.08)] md:gap-4 md:px-7.5 md:py-4 lg:gap-5 lg:rounded-[10px] lg:py-5 xl:hidden">
+      <div className="flex w-full items-center justify-between gap-2 rounded-[8px] bg-[linear-gradient(to_right,#FEF1E8_0%,#FFFFFF_50%,#FEF1E8_100%)] md:gap-4 md:px-7.5 md:py-4 lg:gap-5 lg:rounded-[10px] lg:py-5 xl:hidden">
         <div className="flex min-w-0 flex-1 items-center gap-2 md:gap-4 lg:gap-5">
           {/* Icon */}
           <div className="flex h-[49px] w-[49px] shrink-0 items-center justify-center rounded-full bg-white shadow-[0_2px_8px_rgba(240,90,40,0.12)] md:h-[101px] md:w-[101px] lg:h-[68px] lg:w-[68px]">
@@ -39,10 +39,11 @@ export default function CustomOrderInquiry() {
         </div>
 
         <ActionButton
+        
           title="Send Request"
           cardWidth="h-[25px] w-[95px] md:h-[53px] md:w-[196px] lg:h-[64px] lg:w-[210px]"
           iconCardWidth="h-[20px] w-[20px] md:h-[41px] md:w-[41px] lg:h-[52px] lg:w-[52px]"
-          buttonBg="bg-[#FF4900]"
+          buttonBg="bg-[linear-gradient(90deg,#FF4900_0%,#FF6501_100%)]"
           titleColor="text-white"
           arrowBg="bg-white"
           arrowColor="text-black"
