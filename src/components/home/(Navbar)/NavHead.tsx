cfdbd0ccdeff2994068cd4674962078b413d11ec
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2026-08-27 16:48:14 +0600

    Update: src/components/home/(Navbar)/NavHead.tsx

--- src/components/home/(Navbar)/NavHead.tsx
+++ src/components/home/(Navbar)/NavHead.tsx
@@ -146,7 +146,7 @@ export default function NavHead() {
       />
       <header className="relative bg-white sticky top-0 z-50">
         <div className="relative" onMouseLeave={() => setActiveMenu(null)}>
-          <div className="container-custom flex justify-between items-center px-2.25 md:px-0 py-3.5 md:py-3 xl:py-4.25 3xl:py-[23.5px]">
+          <div className="container-custom flex justify-between items-center px-5 md:px-0 py-3.5 md:py-3 xl:py-4.25 3xl:py-[23.5px]">
             {/* ── Logo ── */}
             <div className="shrink-0 flex items-center justify-start">
               <Link href="/">
